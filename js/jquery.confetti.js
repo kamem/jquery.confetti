5fd98bf6a53fc08c58e8ec881141bcb24d9f3e50
kamem <develo0@gmail.com>
2014-06-28 14:12:48 +0900

    add confetti

---
+++ js/jquery.confetti.js
@@ -0,0 +1,126 @@
+/**
+ *	jQuery confetti.
+ *	jQuery required.
+ *
+ *	* Copyright 2014 (c) kamem
+ *	* http://develo.org/
+ *	* Licensed Under the MIT.
+ *
+ *	Date: 2014.06.28
+ *
+ * jQuery plugin to reproduce the confetti
+ *
+ *	@class confetti
+ */
+
+(function($,global){
+$.fn.confetti = function (options) {
+	var $content = this,
+		c = $.extend({
+			x: $content.offset().left + $content.width() / 2,
+			y: $content.offset().top + $content.height() / 2,
+			num: 20,
+			speedRange: 10,
+			airResistanceNum:  0.95,
+			gravityNum: 0.45,
+			intervalSpeed: 30,
+			complate: false
+		},options),
+
+		random = [],
+		removeNum = 0,
+		isComplate = false;
+
+		className = 'confetti';
+
+	set(c.x,c.y,c.num,c.speedRange);
+	exception(c.airResistanceNum,c.gravityNum,c.intervalSpeed,c.complate);
+
+	/**
+	 * Number of confetti you want to generate
+	 * @method set
+	 * @param {Number} x position to generate a tag in the x-direction
+	 * @param {Number} y position to generate a tag in the y-direction
+	 * @param {Number} num Number of confetti you want to generate
+	 * @param {Number} speedRange range of initial velocity
+	 */
+	function set(x,y,num,speedRange) {
+		for (var i = 0; i < num; i++) {
+			$('body').append('<div class="' + className + i + '"></div>');
+			random[i] = {};
+			random[i].left = Math.random() * (speedRange * 2) - speedRange;
+			random[i].top = Math.random() * (speedRange * 2) - speedRange;
+			random[i].bgColor = '#'+ Math.floor(Math.random() * 0xFFFFFF).toString(16)
+		};
+
+		$('[class*=' + className + ']').css({
+			left: x + 'px',
+			top: y + 'px'
+		});
+	}
+
+	/**
+	 * Number processing execution of confetti
+	 * @method exception
+	 * @param  {Number} airResistanceNum air resistance
+	 * @param  {Number} gravityNum gravity
+	 * @param  {Number} intervalSpeed intervalSpeed
+	 * @param  {Function} complate complate function
+	 */
+	function exception(airResistanceNum,gravityNum,intervalSpeed,complate){
+		$('[class*=' + className + ']').each(function(index) {
+			$(this).css({
+				background: random[index].bgColor
+			});
+			gravity(
+				$(this),
+				random[index].left,
+				random[index].top,
+				airResistanceNum,
+				gravityNum,
+				intervalSpeed,
+				complate
+			);
+		});
+	}
+
+	/**
+	 * The slowing down the speed by gravity and air resistance on the basis of the value of the x direction y direction
+	 * @method gravity
+	 * @param  {Object} $content jQuery Object
+	 * @param  {Number} speedX initial velocity in the x-direction
+	 * @param  {Number} speedY initial velocity in the y-direction
+	 * @param  {Number} airResistanceNum air resistance
+	 * @param  {Number} gravityNum gravity
+	 * @param  {Number} intervalSpeed intervalSpeed
+	 * @param  {Function} complate complate function
+	 */
+	function gravity($content,speedX,speedY,airResistanceNum,gravityNum,intervalSpeed,complate){
+		var timer = setInterval(function() {
+			var leftPosition = $content.position().left,
+				topPosition = $content.position().top;
+
+		    	speedX *= airResistanceNum;
+		    	speedY = speedY * airResistanceNum + gravityNum;
+
+			$content.css({
+				left: leftPosition + speedX + 'px',
+				top: topPosition + speedY + 'px'
+			});
+
+			if($('body').height() < topPosition + speedY) {
+				removeNum++;
+				$content.remove();
+			};
+
+			if(removeNum === c.num) {
+				clearTimeout(timer);
+				if(complate && !isComplate){
+					complate();
+					isComplate = true;
+				}
+			}
+		},intervalSpeed);
+	}
+};
+}(jQuery,this));
